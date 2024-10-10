1f8fce4f56aa38b2254a70ca1968dd3c63faf0f1
muhammadhaseeb5 <muhammadhaseebshahzad5@gmail.com>
2024-10-10 18:44:29 +0500

    Add files via upload

---
+++ myfirstprogram.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    // cout<<"Hello World!";
+    // int n,a,b;
+    // cout<<"Enter Three digit number : ";
+    // cin >>n;
+    // a = n/100;
+    // n = n%100;
+    // b = n/10;
+    // n = n%10;
+    // cout<<"The Reciprocal of three digit no is "<< n<<b<<a;
+    //     if and else if 
+    // int a ,b ;
+    // cout<<"Enter Your age : ";
+    // cin>>a;
+    // if (a<=17 && a>=1)
+    // {
+    //     cout<<"Your age is "<<a << " And You can't vote";
+    // }
+    // else if (a>=18 && a<=70)
+    //  {
+    //     cout<<"Your Age is "<<a<< " And You Can vote";
+    // }   
+    // else {
+    //     cout<<"Invalid Age ";
+    // }
+    //      while loop
+    // int a=1;
+    // while (a<=10)
+    // {
+    //     cout<<" Value is "<< a<<endl;
+    //     a+=1;
+    // }
+    //      Do while 
+    // int a=1;
+    // do
+    // {
+    //     cout<<" Value is "<< a<<endl;
+    //     a+=1;
+    // } while (a<=30);
+    //       for loop
+    // int a,b;
+    // for (a=0; a<=10; a++)
+    // {
+    //     cout<<" value "<< a <<endl;
+    // }
+    //      strings & Arrays
+//  string Data = "My name is Muhammmad Haseeb Shahzad";
+//  cout<<"What's your name "<<endl<<Data[11]<<endl<< Data.size() ; 
+//   string sizeoff [8] = {"haseeb", "shahzad"," hlo"," abd"," umer" } ;
+//   int sizeoff[]={20, 30 ,40 ,50 ,60 ,70};
+// //   cout<<sizeof(sizeoff[8]);
+// int getArrayLength = sizeof(sizeoff) / sizeof(sizeoff[0]);
+//   cout<<getArrayLength; 
+//      Structure
+// Declare a structure named "car"
+// struct car {
+//   string brand;
+//   string model;
+//   int year;
+// };
+//   // Create a car structure and store it in myCar1;
+//   car myCar1;
+//   myCar1.brand = "BMW";
+//   myCar1.model = "X5";
+//   myCar1.year = 1999;
+
+//   // Create another car structure and store it in myCar2;
+//   car myCar2;
+//   myCar2.brand = "Ford";
+//   myCar2.model = "Mustang";
+//   myCar2.year = 1969;
+
+// // Create another car structure and store it in myCar2;
+//   car myCar3;
+//   myCar3.brand= "thar";
+//   myCar3.model= "hyndui";
+//   myCar3.year= 2024; 
+//   // Print the structure members
+//   cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+//   cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+//    cout << myCar3.brand << " " << myCar3.model << " " << myCar3.year << "\n";
+// // int primeNo=1;
+// while (primeNo)
+// {
+//     cout<<primeNo ;
+//     primeNo++;
+// }
+// struct myClassTeacher {
+//    string English ;
+//    string Physics ;
+//    string PF ;
+//    string CS ;
+// }EngT,phyT,PFT,CST;
+// EngT.English = "Miss Sana Sajjad";
+// phyT.Physics = "Sir Umer";
+// PFT.PF = "Sir Usman";
+// CST.CS ="Sir Zohaib";
+// cout<< "BS-CS 1B Teachers "<< endl;
+// cout<< EngT.English << endl << phyT.Physics << endl <<PFT.PF << endl << CST.CS;
+//      create Refrence
+// string food ="PIZZA";
+// string &meal = food;
+// cout<<food<<endl<<meal;
+
+return 0;
+}
+
+
